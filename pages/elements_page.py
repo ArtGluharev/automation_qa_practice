62a5dd66977bc17df20848966f63965d919491a8
ArtGluharev <art96forwork@gmail.com>
2022-12-05 16:09:38 +0700

    update elements tests added test radio button

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,7 +1,7 @@
 import random
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonLocators
 from pages.base_page import BasePage
 
 
@@ -61,3 +61,16 @@ class CheckBoxPage(BasePage):
         for item in result_list:
             data.append(item.text)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonLocators()
+
+    def click_on_the_radio_button(self, choice):
+        choices = {'yes': self.locators.AGREE_RADIO_BUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIO_BUTTON,
+                   'no': self.locators.NO_RADIO_BUTTON, }
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_radio(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT_RADIO)

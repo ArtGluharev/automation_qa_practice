58b97e14ab3be36ac89c2e1a5d8927c433a06d32
ArtGluharev <art96forwork@gmail.com>
2022-12-13 02:12:28 +0700

    added class: -TestAutoCompletePage added tests: -test_fill_multi_autocomplete -test_remove_value_from_multi -test_remove_all_value_from_multi -test_fill_single_autocomplete

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -8,3 +8,12 @@ class AccordianPageLocators:
     SECTION_TWO_CONTENT = (By.CSS_SELECTOR, 'div[id="section2Content"] p')
     SECTION_THREE = (By.CSS_SELECTOR, 'div[id="section3Heading"]')
     SECTION_THREE_CONTENT = (By.CSS_SELECTOR, 'div[id="section3Content"] p')
+
+
+class AutoCompletePageLocators:
+    MULTI_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteMultipleInput"]')
+    MULTI_VALUE = (By.CSS_SELECTOR, 'div[class="css-12jo7m5 auto-complete__multi-value__label"]')
+    MULTI_VALUE_REMOVE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"] svg path')
+    MULTI_ALL_REMOVE = (By.CSS_SELECTOR, 'svg[height="20"][width="20"] path')
+    SINGLE_CONTAINER = (By.CSS_SELECTOR, 'div[class="auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteSingleInput"]')
--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,4 +1,4 @@
-from pages.widgets_page import AccordianPage
+from pages.widgets_page import AccordianPage, AutoCompletePage
 
 
 class TestWidgets:
@@ -9,6 +9,35 @@ class TestWidgets:
             first_title, first_content = accordian_page.check_accordian('first')
             second_title, second_content = accordian_page.check_accordian('second')
             third_title, third_content = accordian_page.check_accordian('third')
-            assert first_title == "What is Lorem Ipsum?" and first_content > 0
-            assert second_title == "Where does it come from?" and second_content > 0
-            assert third_title == "Why do we use it?" and third_content > 0
+            assert first_title == "What is Lorem Ipsum?" and first_content > 0, "Incorrect title or missing text"
+            assert second_title == "Where does it come from?" and second_content > 0, "Incorrect title or missing text"
+            assert third_title == "Why do we use it?" and third_content > 0, "Incorrect title or missing text"
+
+    class TestAutoCompletePage:
+        def test_fill_multi_autocomplete(self, driver):
+            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page.open()
+            colors = autocomplete_page.fill_input_multi()
+            colors_result = autocomplete_page.check_color_in_multi()
+            assert colors == colors_result, "The added colors are missing in the input"
+
+        def test_remove_value_from_multi(self, driver):
+            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page.open()
+            autocomplete_page.fill_input_multi()
+            autocomplete_page.remove_value_from_multi_result()
+            count_value_before, count_value_after = autocomplete_page.remove_value_from_multi_result()
+            assert count_value_before != count_value_after, "The added colors are missing in the input"
+
+        def test_remove_all_value_from_multi(self, driver):
+            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page.open()
+            colors = autocomplete_page.fill_input_multi_and_clear()
+            assert type(colors) is type(None), "Multiple color names form is not empty"
+
+        def test_fill_single_autocomplete(self, driver):
+            autocomplete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            autocomplete_page.open()
+            color = autocomplete_page.fill_input_single()
+            color_result = autocomplete_page.check_color_in_single()
+            assert color == color_result, "The added color is missing in the input"
